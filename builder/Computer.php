925ea19aca8a9a942762d9601d8dc3c8420a42af
Miguel Ceballos <mceballosleonides@gmail.com>
2025-04-14 22:42:25 -0600

    Initial commit: Add builder pattern implementation

---
+++ builder/Computer.php
@@ -0,0 +1,73 @@
+<?php
+
+class Computer
+{
+    public string $cpu;
+    public string $ram;
+    public string $ssd;
+    public string $gpu;
+
+    public function displayConfiguration() : void
+    {
+        echo "CPU: $this->cpu\nRAM: $this->ram\nSSD: $this->ssd\nGPU: $this->gpu\n";
+    }
+}
+
+class ComputerBuilder
+{
+    private Computer $computer;
+
+    public function __construct()
+    {
+        $this->computer = new Computer();
+    }
+
+    public function setCpu(string $cpu) : static
+    {
+        $this->computer->cpu = $cpu;
+        return $this;
+    }
+
+    public function setRam(string $ram) : static
+    {
+        $this->computer->ram = $ram;
+        return $this;
+    }
+
+    public function setSsd(string $ssd) : static
+    {
+        $this->computer->ssd = $ssd;
+        return $this;
+    }
+
+    public function setGpu(string $gpu) : static
+    {
+        $this->computer->gpu = $gpu;
+        return $this;
+    }
+
+    public function build() : Computer
+    {
+        return $this->computer;
+    }
+}
+
+$gamingComputer = new ComputerBuilder();
+
+$gamingComputer
+    ->setCpu("Intel Core i7")
+    ->setRam("16GB")
+    ->setSsd("256GB")
+    ->setGpu("NVIDIA GeForce RTX 3080")
+    ->build()
+    ->displayConfiguration();
+
+$basicComputer = new ComputerBuilder();
+
+$basicComputer
+    ->setCpu("Intel Core i5")
+    ->setRam("8GB")
+    ->setSsd("128GB")
+    ->setGpu("NVIDIA GeForce GTX 1660")
+    ->build()
+    ->displayConfiguration();
